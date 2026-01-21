e05fd52f70a0b27cb5dd028e24be064b463b7ae3
Puntokas <126613054+Puntokas@users.noreply.github.com>
2026-01-21 18:30:59 +0200

    App.js link update

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
   const [products, setProducts] = useState([]);
 
   useEffect(()=>{
-    fetch(`https://enebabackend.onrender.com//list?search=${encodeURIComponent(search)}`)
+    fetch(`https://enebabackend.onrender.com/list?search=${encodeURIComponent(search)}`)
     .then(res => res.json())
     .then(data => setProducts(data)) //console.log("Fetched ", data)})
     .catch(err=>console.error("Error fetching products: ", err));
